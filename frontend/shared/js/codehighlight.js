707f9dbab6f4c22200de6515146aa79223372ce1
Riku Nurminen <riku@nurminen.dev>
2021-06-15 17:10:32 +0300

    Fix highlight.js issue

--- frontend/shared/js/codehighlight.js
+++ frontend/shared/js/codehighlight.js
@@ -29,7 +29,7 @@
 // highlight.js for pages that need code highlighting
 //
 
-import hljs from 'highlight.js/lib/core.js'
+import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript.js'
 
 import 'highlight.js/styles/monokai-sublime.css';
